353dc7649b5ce75b9a6a088fcaf8bcb9c4e798ff
Umesh Choudhary <166589058+ucsirvi@users.noreply.github.com>
2025-04-18 12:02:49 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,33 +8,23 @@ import Navbar from "./components/Navbar";
 function App() {
   const [transactions, setTransactions] = useState([]);
 
-  // Function to handle adding a new transaction
+
   const handleAddTransaction = (newTransaction) => {
     setTransactions((prev) => [...prev, newTransaction]);
   };
 
-  // // Function to handle updating an existing transaction
-  // const handleUpdateTransaction = (updatedTransaction) => {
-  //   setTransactions((prev) =>
-  //     prev.map((transaction) =>
-  //       transaction._id === updatedTransaction._id
-  //         ? updatedTransaction
-  //         : transaction
-  //     )
-  //   );
-  // };
 
   return (
     <div className="bg-gray-900 text-gray-100 min-h-screen">
       <Navbar />
       <div className="container mx-auto p-4">
-        {/* Transaction Form */}
+
         <TransactionForm onAdd={handleAddTransaction} />
 
-        {/* Dashboard */}
+
         <Dashboard transactions={transactions} />
 
-        {/* Transaction List */}
+
         <TransactionList
           onUpdate={(updatedTransaction) => {
             console.log("Transaction Updated in App:", updatedTransaction); // Debug log
